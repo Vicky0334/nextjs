3f3cdc0b79673663b3433a52d37ab8f7165dc32d
Vicky Dhote <dhoteji034@gmail.com>
2024-06-16 00:41:27 +0530

    create Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
 
  const Home = ()=> {
    const [a,seta]=useState(10);
@@ -11,6 +12,7 @@ import React, { useState } from "react";
     <div>
       <h1>A is {a}</h1>
       <button onClick={changeA} className="pr-5 pl-3 p-3 bg-blue-300">ChangeA</button>
+      <Navbar/>
     </div>
   );
 };
