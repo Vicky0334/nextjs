818c43dd760b59c36c00bffcc3147e693f05c804
Vicky Dhote <dhoteji034@gmail.com>
2024-06-15 10:18:28 +0530

    create nextjs app

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useState } from "react";
+
+ const Home = ()=> {
+   const [a,seta]=useState(10);
+   const changeA=()=>{
+   seta(20);
+   };
+  return (
+    <div>
+      <h1>A is {a}</h1>
+      <button onClick={changeA} className="pr-5 pl-3 p-3 bg-blue-300">ChangeA</button>
+    </div>
+  );
+};
+  
+export default Home;
